b850dc48b2d092f8037b2b9386be61b0c794101d
eugeous <121101254+Eugeou@users.noreply.github.com>
2023-06-28 21:31:06 +0700

    Add Arrow (intro scene) to tick the selected game play in intro scene

---
+++ Mario Game 21522495/05-SceneManager/IntroArrow.h
@@ -0,0 +1,27 @@
+﻿#pragma once
+#define ID_ANI_ARROW_INTRO 400010
+#define ID_ANI_ARROW_INTRO_CHANGE_SCENE 400020
+
+#define ARROW_NOT_RENDER 0
+#define ARROW_UP 1
+#define ARROW_DOWN 2
+
+//nút mũi tên để chọn chế độ trong intro scene
+class CIntroArrow
+{
+private:
+	float x, y;
+
+public:
+	CIntroArrow() {
+		x = 0;
+		y = 0;
+	}
+	void Render(float x, float y);
+	void RenderChangeScene(float x, float y);
+	float GetY() { return y; }
+	void SetY(float y) {
+		this->y = y;
+	}
+};
+
